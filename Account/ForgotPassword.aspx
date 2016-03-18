b7e7ca99d33f0092de37ffc9be62783d95442ab1
vergil1217 <vergil1217@gmail.com>
2016-03-18 17:30:20 +0800

    Master Copy

---
+++ Account/ForgotPassword.aspx
@@ -0,0 +1,79 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/EWSD.Master" AutoEventWireup="true" CodeBehind="ForgotPassword.aspx.cs" Inherits="EWSD.Account.ForgotPassword" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="/css/Site.css" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <h3>Forgot Password</h3>
+    <div class="jumbotron">
+        <p class="text-success">
+            <asp:Literal ID="literalActionSuccessful" runat="server"></asp:Literal>
+        </p>
+        <p class="text-danger">
+            <asp:Literal ID="literalActionFailure" runat="server"></asp:Literal>
+        </p><br />
+        <asp:Panel runat="server" ID="panelSecQA">
+            <p>
+                Please provide your security question and answer.
+            </p><br />
+            <table>
+                <tr style="font-size:18px;">
+                    <td>Username:</td>
+                    <td><asp:TextBox ID="fieldUsername" runat="server" Width="100%"></asp:TextBox></td>
+                    <td><asp:RequiredFieldValidator runat="server" ControlToValidate="fieldUsername" CssClass="text-danger" Display="Dynamic" ErrorMessage="Username is required." /></td>
+                </tr>
+                <tr style="font-size:18px;">
+                    <td>Security Question:</td>
+                    <td>
+                        <asp:DropDownList ID="comboSecurityQuestion" runat="server">
+                            <asp:ListItem>Select a question</asp:ListItem>
+                            <asp:ListItem>What is your childhood friend's name?</asp:ListItem>
+                            <asp:ListItem>What is your favourite food?</asp:ListItem>
+                            <asp:ListItem>Who is your favourite superhero?</asp:ListItem>
+                            <asp:ListItem>What is your mother's maiden name?</asp:ListItem>
+                            <asp:ListItem>Where did you had your first kiss?</asp:ListItem>
+                            <asp:ListItem>What is the name of your secondary school's 3rd Year teacher?</asp:ListItem>
+                            <asp:ListItem>Where did you get married?</asp:ListItem>
+                            <asp:ListItem>How did you meet your spouse?</asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                    <td>
+                        <asp:RequiredFieldValidator runat="server" ControlToValidate="comboSecurityQuestion" CssClass="text-danger" Display="Dynamic" InitialValue="Select a question" ErrorMessage="Please select a question." />
+                    </td>
+                </tr>
+                <tr style="font-size:18px;">
+                    <td>Security Answer:</td>
+                    <td><asp:TextBox ID="fieldSecurityAnswer" runat="server" Width="100%"></asp:TextBox></td>
+                    <td><asp:RequiredFieldValidator runat="server" ControlToValidate="fieldSecurityAnswer" CssClass="text-danger" Display="Dynamic" ErrorMessage="Security answer is required." /></td>
+                    <td><asp:Button ID="bAuthSecQA" runat="server" Text="Submit" OnClick="bAuthSecQA_Click" /></td>
+                </tr>
+            </table>
+        </asp:Panel>
+        <asp:Panel runat="server" ID="panelResetPassword" Visible="false">
+            <p>
+                Please enter your new password.
+            </p><br />
+            <table>
+                <tr style="font-size:18px;">
+                    <td>New Password:</td>
+                    <td><asp:TextBox ID="fieldNewPassword" runat="server" TextMode="Password"></asp:TextBox></td>
+                    <td><asp:RequiredFieldValidator runat="server" ControlToValidate="fieldNewPassword" CssClass="text-danger" Display="Dynamic" ErrorMessage="Password is required." /></td>
+                </tr>
+                <tr style="font-size:18px;">
+                    <td>Confirm Password:</td>
+                    <td><asp:TextBox ID="fieldConfirmNewPassword" runat="server" TextMode="Password"></asp:TextBox></td>
+                    <td>
+                        <asp:RequiredFieldValidator runat="server" ControlToValidate="fieldConfirmNewPassword" CssClass="text-danger" Display="Dynamic" ErrorMessage="Confirmation password is required." />
+                        <asp:CompareValidator runat="server" ControlToCompare="fieldNewPassword" ControlToValidate="fieldConfirmNewPassword" CssClass="text-danger" Display="Dynamic" ErrorMessage="The password and confirmation password do not match." />
+                    </td>
+                </tr>
+                <tr style="font-size:18px;">
+                    <td></td>
+                    <td>
+                        <asp:Button ID="bResetPassword" runat="server" Text="Reset Password" OnClick="bResetPassword_Click" />
+                    </td>
+                </tr>
+            </table>
+        </asp:Panel>
+    </div>
+</asp:Content>
---
+++ Account/ForgotPassword.aspx.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EWSD.Account
+{
+    public partial class ForgotPassword : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void bAuthSecQA_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                Session["usernameForgot"] = fieldUsername.Text;
+
+                literalActionSuccessful.Text = "";
+                literalActionFailure.Text = "";
+
+                using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.Connection = conn;
+
+                        cmd.CommandText = "SELECT security_question, security_answer FROM staff WHERE username = @username";
+                        cmd.Prepare();
+
+                        cmd.Parameters.AddWithValue("@username", fieldUsername.Text);
+
+                        conn.Open();
+
+                        string secQ = "";
+                        string secA = "";
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                secQ = reader.GetString(0);
+                                secA = reader.GetString(1);
+                            }
+                        }
+
+                        if (secQ.Equals("") && secA.Equals(""))
+                        {
+                            literalActionFailure.Text = "Password reset fail. Reason: Username doesn't exist.";
+                            fieldUsername.Text = "";
+                            fieldSecurityAnswer.Text = "";
+                            comboSecurityQuestion.SelectedIndex = 0;
+                            conn.Close();
+                            return;
+                        }
+
+                        string secAnsHash = "";
+                        using (SHA1 pw = new SHA1CryptoServiceProvider())
+                        {
+                            secAnsHash = BitConverter.ToString(pw.ComputeHash(Encoding.UTF8.GetBytes(fieldSecurityAnswer.Text))).Replace("-", "").ToLower();
+                        }
+
+                        if (secQ.Equals(comboSecurityQuestion.SelectedItem.Text) && secA.Equals(secAnsHash))
+                        {
+                            panelResetPassword.Visible = true;
+                            panelSecQA.Visible = false;
+                        }
+                        else
+                        {
+                            literalActionFailure.Text = "Password reset fail. Reason: Security Question and Answer mismatch.";
+                            conn.Close();
+                            fieldUsername.Text = "";
+                            fieldSecurityAnswer.Text = "";
+                            comboSecurityQuestion.SelectedIndex = 0;
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void bResetPassword_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                literalActionSuccessful.Text = "";
+                literalActionFailure.Text = "";
+
+                using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.Connection = conn;
+                        cmd.CommandText = "UPDATE staff SET pw = @newPassword WHERE username = @username";
+                        cmd.Prepare();
+
+                        string newPwHash = "";
+                        using (SHA1 pw = new SHA1CryptoServiceProvider())
+                        {
+                            newPwHash = BitConverter.ToString(pw.ComputeHash(Encoding.UTF8.GetBytes(fieldNewPassword.Text))).Replace("-", "").ToLower();
+                        }
+
+                        cmd.Parameters.AddWithValue("@newPassword", newPwHash);
+                        cmd.Parameters.AddWithValue("@username", Session["usernameForgot"].ToString());
+
+                        Session.Remove("usernameForgot");
+
+                        fieldNewPassword.Enabled = false;
+                        fieldConfirmNewPassword.Enabled = false;
+
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                        literalActionSuccessful.Text = "Password reset successful. You will be redirected to the login page in 3 seconds.";
+                        Response.AddHeader("REFRESH", "3;URL=Login.aspx");
+                        conn.Close();
+                    }
+                }
+            }
+        }
+    }
+}
