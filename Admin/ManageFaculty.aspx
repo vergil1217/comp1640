ccaf01af382710d5e524fcd88fa2c658855a5abb
vergil1217 <vergil1217@gmail.com>
2016-03-23 17:36:50 +0800

    Master Copy

---
+++ Admin/ManageFaculty.aspx
@@ -0,0 +1,78 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/EWSD.Master" AutoEventWireup="true" CodeBehind="ManageFaculty.aspx.cs" Inherits="EWSD.Admin.ManageFaculty" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="/css/Site.css" />
+    <script runat="server">
+        protected void bHidePanelAddFaculty_Click(object sender, EventArgs e)
+        {
+            if (panelAddFaculty.Visible)
+            {
+                panelAddFaculty.Visible = false;
+                bHidePanelAddFaculty.Text = "Show Panel";
+            }
+            else
+            {
+                panelAddFaculty.Visible = true;
+                panelRemoveFaculty.Visible = false;
+                bHidePanelAddFaculty.Text = "Hide Panel";
+                bHidePanelRemoveFaculties.Text = "Show Panel";
+            }
+        }
+
+        protected void bHidePanelRemoveFaculties_Click(object sender, EventArgs e)
+        {
+            if (panelRemoveFaculty.Visible)
+            {
+                panelRemoveFaculty.Visible = false;
+                bHidePanelRemoveFaculties.Text = "Show Panel";
+            }
+            else
+            {
+                panelRemoveFaculty.Visible = true;
+                panelAddFaculty.Visible = false;
+                bHidePanelRemoveFaculties.Text = "Hide Panel";
+                bHidePanelAddFaculty.Text = "Show Panel";
+            }
+        }
+    </script>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="jumbotron">
+        <h3>Manage Faculties</h3>
+        <p style="font-size:18px;">
+            <span class="text-danger"><asp:Literal ID="literalActionFailure" runat="server"></asp:Literal></span><br />
+            <span class="text-success"><asp:Literal ID="literalActionSuccess" runat="server"></asp:Literal></span>
+        </p>
+        <fieldset>
+            <legend>Add Faculties&nbsp;&nbsp;&nbsp;&nbsp;<asp:Button ID="bHidePanelAddFaculty" runat="server" Text="Hide Panel" CausesValidation="false" OnClick="bHidePanelAddFaculty_Click" /></legend>
+            <asp:Panel runat="server" ID="panelAddFaculty">
+                <table>
+                    <tr style="font-size:18px;">
+                        <td>Faculty Code:</td>
+                        <td><asp:TextBox ID="fieldFacultyCode" runat="server"></asp:TextBox></td>
+                        <td><asp:RequiredFieldValidator runat="server" ControlToValidate="fieldFacultyCode" CssClass="text-danger" Display="Dynamic" ErrorMessage="Faculty Code is required." /></td>
+                    </tr>
+                    <tr style="font-size:18px;">
+                        <td>Faculty Name:</td>
+                        <td><asp:TextBox ID="fieldFacultyName" runat="server" Width="400px"></asp:TextBox></td>
+                        <td><asp:RequiredFieldValidator runat="server" ControlToValidate="fieldFacultyName" CssClass="text-danger" Display="Dynamic" ErrorMessage="Faculty Name is required." /></td>
+                        <td><asp:Button ID="bAddFaculty" runat="server" Text="Add Faculty" OnClick="bAddFaculty_Click" /></td>
+                    </tr>
+                </table>
+            </asp:Panel>
+        </fieldset>
+        <br /><br />
+        <fieldset>
+            <legend>Remove Faculties&nbsp;&nbsp;&nbsp;&nbsp;<asp:Button ID="bHidePanelRemoveFaculties" runat="server" Text="Show Panel" CausesValidation="false" OnClick="bHidePanelRemoveFaculties_Click" /></legend>
+            <asp:Panel runat="server" ID="panelRemoveFaculty" Visible="false">
+                <table>
+                    <tr style="font-size:18px;">
+                        <td>Removable Faculties: </td>
+                        <td><asp:DropDownList ID="comboRemovableFaculties" runat="server"></asp:DropDownList></td>
+                        <td><asp:Button ID="bRemoveFaculty" runat="server" Text="Remove Faculty" OnClick="bRemoveFaculty_Click" /></td>
+                    </tr>
+                </table>
+            </asp:Panel>
+        </fieldset>
+    </div>
+</asp:Content>
---
+++ Admin/ManageFaculty.aspx.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EWSD.Admin
+{
+    public partial class ManageFaculty : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            if (!IsPostBack)
+            {
+                ArrayList arrUnavailableFaculties = new ArrayList();
+
+                using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.Connection = conn;
+                        cmd.CommandText = "SELECT faculty_code FROM faculty INNER JOIN course ON faculty_code = course.registered_faculty";
+                        cmd.Prepare();
+
+                        conn.Open();
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                arrUnavailableFaculties.Add(reader.GetString(0));
+                            }
+                        }
+
+                        cmd.CommandText = "SELECT * FROM faculty";
+                        cmd.Prepare();
+
+                        using(SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            bool isUnavailable;
+
+                            while (reader.Read())
+                            {
+                                isUnavailable = false;
+
+                                foreach(string str in arrUnavailableFaculties)
+                                {
+                                    if (reader.GetString(0).Equals(str))
+                                    {
+                                        isUnavailable = true;
+                                        break;
+                                    }
+                                }
+
+                                if (!isUnavailable)
+                                {
+                                    comboRemovableFaculties.Items.Add(reader.GetString(0) + " - " + reader.GetString(1));
+                                }
+                            }
+                        }
+
+                        if(comboRemovableFaculties.Items.Count < 1)
+                        {
+                            comboRemovableFaculties.Items.Add("No Removable Faculties");
+                            bRemoveFaculty.Enabled = false;
+                        }
+                    }
+                }
+            }
+            
+        }
+
+        protected void bAddFaculty_Click(object sender, EventArgs e)
+        {
+            literalActionFailure.Text = "";
+            literalActionSuccess.Text = "";
+
+            if (Page.IsValid)
+            {
+                try
+                {
+                    using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+                    {
+                        using (SqlCommand cmd = new SqlCommand())
+                        {
+                            cmd.Connection = conn;
+                            cmd.CommandText = "INSERT INTO faculty VALUES (@facultyCode, @facultyName, NULL, NULL)";
+                            cmd.Prepare();
+
+                            cmd.Parameters.AddWithValue("@facultyCode", fieldFacultyCode.Text);
+                            cmd.Parameters.AddWithValue("@facultyName", fieldFacultyName.Text);
+
+                            conn.Open();
+
+                            cmd.ExecuteNonQuery();
+
+                            conn.Close();
+
+                            literalActionSuccess.Text = "Faculty successfully added. Page will refresh in 3 seconds.";
+                            fieldFacultyCode.Text = "";
+                            fieldFacultyName.Text = "";
+                            Response.AddHeader("REFRESH", "3;");
+                        }
+                    }
+                }
+                catch(SqlException ex)
+                {
+                    literalActionFailure.Text = "Faculty addition failed. Reason: " + ex.Message;
+                    fieldFacultyCode.Text = "";
+                    fieldFacultyName.Text = "";
+                }
+            }
+        }
+
+        protected void bRemoveFaculty_Click(object sender, EventArgs e)
+        {
+            literalActionFailure.Text = "";
+            literalActionSuccess.Text = "";
+
+            using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "DELETE FROM faculty WHERE faculty_code = @facultyCode";
+                    cmd.Prepare();
+
+                    cmd.Parameters.AddWithValue("@facultyCode", comboRemovableFaculties.SelectedItem.Text.Split(" - ".ToCharArray())[0]);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+
+                    literalActionSuccess.Text = "Faculty successfully removed. Page will refresh in 3 seconds.";
+                    Response.AddHeader("REFRESH", "3;");
+                }
+            }
+        }
+    }
+}
