80d7b5ea51e14e24657a3eb6a06f7a66620ac315
vergil1217 <vergil1217@gmail.com>
2016-03-23 22:43:10 +0800

    Master Copy

---
+++ Admin/ManageCourse.aspx
@@ -0,0 +1,98 @@
+﻿<%@ Page Title="Manage Course" Language="C#" MasterPageFile="~/EWSD.Master" AutoEventWireup="true" CodeBehind="ManageCourse.aspx.cs" Inherits="EWSD.Admin.ManageCourse" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="/css/Site.css" />
+    <script runat="server">
+        protected void bHidePanelAddCourse_Click(object sender, EventArgs e)
+        {
+            if (panelAddCourse.Visible)
+            {
+                panelAddCourse.Visible = false;
+                bHidePanelAddCourse.Text = "Show Panel";
+            }
+            else
+            {
+                panelAddCourse.Visible = true;
+                panelRemoveCourse.Visible = false;
+                bHidePanelAddCourse.Text = "Hide Panel";
+                bHidePanelRemoveCourse.Text = "Show Panel";
+            }
+        }
+
+        protected void bHidePanelRemoveCourse_Click(object sender, EventArgs e)
+        {
+            if (panelRemoveCourse.Visible)
+            {
+                panelRemoveCourse.Visible = false;
+                bHidePanelRemoveCourse.Text = "Show Panel";
+            }
+            else
+            {
+                panelRemoveCourse.Visible = true;
+                panelAddCourse.Visible = false;
+                bHidePanelRemoveCourse.Text = "Hide Panel";
+                bHidePanelAddCourse.Text = "Show Panel";
+            }
+        }
+    </script>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="jumbotron">
+        <h3>Manage Course</h3>
+        <p style="font-size:18px;">
+            <span class="text-danger"><asp:Literal ID="literalActionFailure" runat="server"></asp:Literal></span><br />
+            <span class="text-success"><asp:Literal ID="literalActionSuccess" runat="server"></asp:Literal></span>
+        </p>
+        <br />
+        <fieldset>
+            <legend>Add Course&nbsp;&nbsp;&nbsp;&nbsp;<asp:Button ID="bHidePanelAddCourse" runat="server" Text="Hide Panel" CausesValidation="false" OnClick="bHidePanelAddCourse_Click" /></legend>
+            <asp:Panel runat="server" ID="panelAddCourse">
+                <table>
+                    <tr style="font-size:18px;">
+                        <td>Course Code: </td>
+                        <td><asp:TextBox ID="fieldCourseCode" runat="server" Width="400px"></asp:TextBox></td>
+                        <td><asp:RequiredFieldValidator runat="server" ControlToValidate="fieldCourseCode" CssClass="text-danger" Display="Dynamic" ErrorMessage="Course Code is required." /></td>
+                    </tr>
+                    <tr style="font-size:18px;">
+                        <td>Course Title: </td>
+                        <td><asp:TextBox ID="fieldCourseTitle" runat="server" Width="400px"></asp:TextBox></td>
+                        <td><asp:RequiredFieldValidator runat="server" ControlToValidate="fieldCourseTitle" CssClass="text-danger" Display="Dynamic" ErrorMessage="Course Title is required." /></td>
+                    </tr>
+                    <tr style="font-size:18px;">
+                        <td>Course Prefix: </td>
+                        <td><asp:TextBox ID="fieldCoursePrefix" runat="server"></asp:TextBox></td>
+                        <td><asp:RequiredFieldValidator runat="server" ControlToValidate="fieldCoursePrefix" CssClass="text-danger" Display="Dynamic" ErrorMessage="Course Prefix is required." /></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td style="font-size:14px;color:blue;">*Prefix for subjects e.g. COMP1440, Prefix: COMP</td>
+                    </tr>
+                    <tr style="font-size:18px;">
+                        <td>Parent Faculty: </td>
+                        <td><asp:DropDownList ID="comboParentFaculty" runat="server"></asp:DropDownList></td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td style="text-align:center"><asp:Button ID="bAddCourse" runat="server" Text="Add Course" OnClick="bAddCourse_Click" /></td>
+                    </tr>
+                </table>
+            </asp:Panel>
+        </fieldset>
+        <br /><br />
+        <fieldset>
+            <legend>Remove Course&nbsp;&nbsp;&nbsp;&nbsp;<asp:Button ID="bHidePanelRemoveCourse" runat="server" Text="Show Panel" CausesValidation="false" OnClick="bHidePanelRemoveCourse_Click" /></legend>
+            <asp:Panel runat="server" ID="panelRemoveCourse" Visible="false">
+                <table>
+                    <tr style="font-size:18px;">
+                        <td>Removable Courses: </td>
+                        <td><asp:DropDownList ID="comboRemovableCourses" runat="server"></asp:DropDownList></td>
+                        <td><asp:Button ID="bRemoveCourse" runat="server" Text="Remove Course" OnClick="bRemoveCourse_Click" /></td>
+                    </tr>
+                </table>
+            </asp:Panel>
+        </fieldset>
+    </div>
+</asp:Content>
---
+++ Admin/ManageCourse.aspx.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EWSD.Admin
+{
+    public partial class ManageCourse : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.Connection = conn;
+                        cmd.CommandText = "SELECT * FROM faculty";
+                        cmd.Prepare();
+
+                        conn.Open();
+                        using(SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                comboParentFaculty.Items.Add(reader.GetString(0) + " - " + reader.GetString(1));
+                            }
+                        }
+
+                        ArrayList arrUnavailableCourses = new ArrayList();
+
+                        //load remove course drop down list
+                        cmd.CommandText = "SELECT DISTINCT course_code FROM course INNER JOIN coursework ON course_code = coursework.parent_course";
+                        cmd.Prepare();
+
+                        using(SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                arrUnavailableCourses.Add(reader.GetString(0));
+                            }
+                        }
+
+                        cmd.CommandText = "SELECT * FROM course";
+                        cmd.Prepare();
+
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            bool isUnavailable;
+
+                            while (reader.Read())
+                            {
+                                isUnavailable = false;
+
+                                foreach(string str in arrUnavailableCourses)
+                                {
+                                    if (reader.GetString(0).Equals(str))
+                                    {
+                                        isUnavailable = true;
+                                        break;
+                                    }
+                                }
+
+                                if (!isUnavailable)
+                                {
+                                    comboRemovableCourses.Items.Add(reader.GetString(0) + " - " + reader.GetString(1));
+                                }
+                            }
+                        }
+
+                        if(comboRemovableCourses.Items.Count < 1)
+                        {
+                            comboRemovableCourses.Items.Add("No Removable Courses");
+                            bRemoveCourse.Enabled = false;
+                        }
+
+                        conn.Close();
+                    }
+                }
+            }
+        }
+
+        protected void bAddCourse_Click(object sender, EventArgs e)
+        {
+            literalActionFailure.Text = "";
+            literalActionSuccess.Text = "";
+
+            if (Page.IsValid)
+            {
+                try
+                {
+                    using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+                    {
+                        using (SqlCommand cmd = new SqlCommand())
+                        {
+                            cmd.Connection = conn;
+                            cmd.CommandText = "INSERT INTO course VALUES (@courseCode, @courseTitle, @coursePrefix, @parentFaculty, NULL, NULL)";
+                            cmd.Prepare();
+
+                            cmd.Parameters.AddWithValue("@courseCode", fieldCourseCode.Text);
+                            cmd.Parameters.AddWithValue("@courseTitle", fieldCourseTitle.Text);
+                            cmd.Parameters.AddWithValue("@coursePrefix", fieldCoursePrefix.Text);
+                            cmd.Parameters.AddWithValue("@parentFaculty", comboParentFaculty.SelectedItem.Text.Split(" - ".ToCharArray())[0]);
+
+                            conn.Open();
+                            cmd.ExecuteNonQuery();
+                            conn.Close();
+                            fieldCourseCode.Text = "";
+                            fieldCourseTitle.Text = "";
+                            fieldCoursePrefix.Text = "";
+
+                            literalActionSuccess.Text = "Course successfully added. Page will refresh in 3 seconds.";
+                            Response.AddHeader("REFRESH", "3;");
+                        }
+                    }
+                }
+                catch(SqlException ex)
+                {
+                    literalActionFailure.Text = "Course addition failed. Reason: " + ex.Message;
+                }
+            }
+        }
+
+        protected void bRemoveCourse_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "DELETE FROM course WHERE course_code = @courseCode";
+                    cmd.Prepare();
+
+                    cmd.Parameters.AddWithValue("@courseCode", comboRemovableCourses.SelectedItem.Text.Split(" - ".ToCharArray())[0]);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+
+                    literalActionSuccess.Text = "Course removal successful. Page will refresh in 3 seconds.";
+                    Response.AddHeader("REFRESH", "3;");
+                }
+            }
+        }
+    }
+}
