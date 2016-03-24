52c45302ffa2201f14db68f62f8ec7178fefcc93
vergil1217 <vergil1217@gmail.com>
2016-03-25 02:15:35 +0800

    Master Copy

---
+++ Admin/RemoveFacultyRole.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/EWSD.Master" AutoEventWireup="true" CodeBehind="RemoveFacultyRole.aspx.cs" Inherits="EWSD.Admin.RemoveFacultyRole" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="/css/Site.css" />
+    <script runat="server">
+        protected void bRemoveStaff_Click(object sender, EventArgs e)
+        {
+            bRemoveStaff.Visible = false;
+            bConfirmRemoveStaff.Visible = true;
+        }
+    </script>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <h3>Remove <asp:Label ID="labelRole" runat="server" Text=""></asp:Label></h3>
+    <div class="jumbotron">
+        <p style="font-size:18px;">
+            <span class="text-danger"><asp:Literal ID="literalWarning" runat="server"></asp:Literal></span><br />
+            <span class="text-success"><asp:Literal ID="literalActionSuccess" runat="server"></asp:Literal></span>
+        </p>
+        <fieldset>
+            <asp:Panel runat="server" ID="panelSelectFaculty">
+                <table>
+                    <tr style="font-size:18px;">
+                        <td>Select Faculty: </td>
+                        <td><asp:DropDownList ID="comboFaculties" runat="server"></asp:DropDownList></td>
+                        <td><asp:Button ID="bSelectFaculty" runat="server" Text="Select Faculty" OnClick="bSelectFaculty_Click" /></td>
+                    </tr>
+                </table>
+            </asp:Panel>
+        </fieldset>
+        <br />
+        <fieldset>
+            <asp:Panel runat="server" ID="panelRemoveStaff" Visible="false">
+                <table>
+                    <tr style="font-size:18px;">
+                        <td>Confirm Removal?</td>
+                        <td style="font-size:14px;padding:10px;color:blue;">
+                            Faculty: <asp:Literal ID="literalConfirmFaculty" runat="server"></asp:Literal><br />
+                            Staff Name: <asp:Literal ID="literalStaffName" runat="server"></asp:Literal><br />
+                        </td>
+                        <td><asp:Button ID="bRemoveStaff" runat="server" Text="Remove" OnClick="bRemoveStaff_Click" /></td>
+                        <td><asp:Button ID="bConfirmRemoveStaff" runat="server" Text="Confirm Removal" OnClick="bConfirmRemoveStaff_Click" Visible="false"/></td>
+                    </tr>
+                </table>
+            </asp:Panel>
+        </fieldset>
+    </div>
+    <asp:TextBox ID="fieldStaffId" runat="server" Visible="false" ReadOnly="true"></asp:TextBox>
+</asp:Content>
---
+++ Admin/RemoveFacultyRole.aspx.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EWSD.Admin
+{
+    public partial class RemoveFacultyRole : System.Web.UI.Page
+    {
+        private string role;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                role = Request.QueryString["role"];
+
+                if (role.Equals("pvc"))
+                {
+                    labelRole.Text = "Pro-Vice Chancellor";
+                }
+                else if (role.Equals("dlt"))
+                {
+                    labelRole.Text = "Director of Learning & Quality";
+                }
+
+                using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.Connection = conn;
+                        if (role.Equals("pvc"))
+                        {
+                            cmd.CommandText = "SELECT faculty_code, faculty_name FROM faculty WHERE pro_vice_chancellor IS NOT NULL;";
+                        }
+                        else if (role.Equals("dlt"))
+                        {
+                            cmd.CommandText = "SELECT faculty_code, faculty_name FROM faculty WHERE director_learning_quality IS NOT NULL;";
+                        }
+                        cmd.Prepare();
+
+                        conn.Open();
+                        using(SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                comboFaculties.Items.Add(reader.GetString(0) + " - " + reader.GetString(1));
+                            }
+                        }
+                        conn.Close();
+
+                        if (comboFaculties.Items.Count < 1)
+                        {
+                            comboFaculties.Items.Add("No faculties");
+                            bSelectFaculty.Enabled = false;
+                        }
+                        
+                    }
+                }
+            }
+        }
+
+        protected void bSelectFaculty_Click(object sender, EventArgs e)
+        {
+            role = Request.QueryString["role"];
+            string facultyCode = comboFaculties.SelectedItem.Text.Split(" - ".ToCharArray())[0];
+
+            using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    conn.Open();
+
+                    cmd.CommandText = "SELECT * FROM faculty WHERE faculty.faculty_code = @facultyCode";
+                    cmd.Parameters.Add("@facultyCode", SqlDbType.VarChar, 50).Value = facultyCode;
+                    cmd.Prepare();
+
+                    int staffId = 0;
+                    
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        reader.Read();
+                        if (role.Equals("pvc"))
+                        {
+                            staffId = reader.GetInt32(2);
+                        }
+                        else if (role.Equals("dlt"))
+                        {
+                            staffId = reader.GetInt32(3);
+                        }
+                    }
+                    fieldStaffId.Text = staffId.ToString();
+
+                    cmd.Parameters.Clear();
+
+                    cmd.CommandText = "SELECT * FROM staff WHERE staff.staff_id = @staffId";
+                    cmd.Parameters.Add("@staffId", SqlDbType.Int).Value = staffId;
+                    cmd.Prepare();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        reader.Read();
+                        literalConfirmFaculty.Text = comboFaculties.SelectedItem.Text;
+                        literalStaffName.Text = reader.GetString(3) + " " + reader.GetString(4);
+                    }
+                }
+            }
+
+            panelSelectFaculty.Visible = false;
+            panelRemoveStaff.Visible = true;
+        }
+
+        protected void bConfirmRemoveStaff_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    role = Request.QueryString["role"];
+                    string facultyCode = comboFaculties.SelectedItem.Text.Split(" - ".ToCharArray())[0];
+
+                    cmd.Connection = conn;
+                    if (role.Equals("pvc"))
+                    {
+                        cmd.CommandText = "UPDATE faculty SET pro_vice_chancellor = NULL WHERE faculty_code = @facultyCode";
+                    }
+                    else if (role.Equals("dlt"))
+                    {
+                        cmd.CommandText = "UPDATE faculty SET director_learning_quality = NULL WHERE faculty_code = @facultyCode";
+                    }
+                    cmd.Parameters.Add("@facultyCode", SqlDbType.VarChar, 50).Value = facultyCode;
+                    conn.Open();
+                    cmd.Prepare();
+                    
+                    cmd.ExecuteNonQuery();
+
+                    cmd.Parameters.Clear();
+
+                    cmd.CommandText = "UPDATE staff SET user_role = NULL WHERE staff_id = @staffId";
+                    cmd.Prepare();
+
+                    cmd.Parameters.AddWithValue("@staffId", int.Parse(fieldStaffId.Text));
+
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+
+                    literalActionSuccess.Text = labelRole.Text + " successfully removed. You will be redirected to the Admin Homepage in 3 seconds.";
+                    Response.AddHeader("REFRESH", "3;URL=AdminHome.aspx");
+                }
+            }
+        }
+    }
+}
