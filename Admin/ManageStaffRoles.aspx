29187814e5a998aa9911649fdfbcd717ca0a0895
vergil1217 <vergil1217@gmail.com>
2016-04-04 03:46:00 +0800

    Master Copy

---
+++ Admin/ManageStaffRoles.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/EWSD.Master" AutoEventWireup="true" CodeBehind="ManageStaffRoles.aspx.cs" Inherits="EWSD.Admin.ManageStaffRoles" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link rel="stylesheet" type="text/css" href="/css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="/css/Site.css" />
+    <script runat="server">
+    </script>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <h3>Manage Staff Roles</h3>
+    <div class="jumbotron">
+        <p style="font-size:18px;">
+            <span class="text-danger"><asp:Literal ID="literalActionFailure" runat="server"></asp:Literal></span><br/>
+            <span class="text-success"><asp:Literal ID="literalActionSuccess" runat="server"></asp:Literal></span>
+        </p>
+        <asp:Panel runat="server" ID="panelSelectStaff">
+            <fieldset>
+                <legend>Staff Selection</legend>
+                    <table>
+                        <tr style="font-size:18px;">
+                            <td>Select Staff: </td>
+                            <td><asp:DropDownList ID="comboStaff" runat="server"></asp:DropDownList></td>
+                            <td><asp:Button ID="bSelectStaff" runat="server" Text="Select Staff" OnClick="bSelectStaff_Click"/></td>
+                        </tr>
+                    </table>
+            </fieldset>
+        </asp:Panel>
+        <asp:Panel runat="server" ID="panelAssignRoleToStaff" Visible="false">
+            <fieldset>
+                <legend>Assign Role</legend>
+                <table>
+                    <tr style="font-size:18px;">
+                        <td>Staff Name:</td>
+                        <td><asp:Label ID="labelStaffName" runat="server" Text=""></asp:Label></td>
+                    </tr>
+                    <tr style="font-size:18px;">
+                        <td>Staff Role:</td>
+                        <td><asp:DropDownList ID="comboRole" runat="server"></asp:DropDownList></td>
+                        <td><asp:Button ID="bAssignRole" runat="server" Text="Confirm Assignment" OnClick="bAssignRole_Click" /></td>
+                    </tr>
+                </table>
+            </fieldset>
+        </asp:Panel>
+        <asp:TextBox ID="fieldUserRole" runat="server" ReadOnly="True" Visible="False"></asp:TextBox>
+    </div>
+</asp:Content>
---
+++ Admin/ManageStaffRoles.aspx.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EWSD.Admin
+{
+    public partial class ManageStaffRoles : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.Connection = conn;
+                        cmd.CommandText = "SELECT * FROM staff WHERE user_role <> 5";
+                        cmd.Prepare();
+
+                        conn.Open();
+                        using(SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                ListItem item = new ListItem(reader.GetString(3) + " " + reader.GetString(4), reader.GetInt32(0).ToString());
+                                comboStaff.Items.Add(item);
+                            }
+                        }
+
+                        conn.Close();
+                        if(comboStaff.Items.Count < 1)
+                        {
+                            comboStaff.Items.Add("No Staffs");
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void bSelectStaff_Click(object sender, EventArgs e)
+        {
+            panelSelectStaff.Visible = false;
+            panelAssignRoleToStaff.Visible = true;
+            
+            using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "SELECT * FROM staff WHERE staff_id = @staffId";
+                    cmd.Prepare();
+
+                    cmd.Parameters.AddWithValue("@staffId", int.Parse(comboStaff.SelectedItem.Value));
+
+                    conn.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            fieldUserRole.Text = reader.GetInt32(10).ToString();
+                        }
+                    }
+
+                    cmd.Parameters.Clear();
+
+                    cmd.CommandText = "SELECT * FROM roles";
+                    cmd.Prepare();
+                    
+                    using(SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            ListItem item = new ListItem(reader.GetString(1), reader.GetInt32(0).ToString());
+                            if (reader.GetInt32(0) == int.Parse(fieldUserRole.Text))
+                            {
+                                item.Selected = true;
+                            }
+                            comboRole.Items.Add(item);
+                        }
+                    }
+                }
+            }
+            
+        }
+
+        protected void bAssignRole_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "UPDATE staff SET user_role = @role WHERE staff_id = @staffId";
+                    cmd.Prepare();
+
+                    cmd.Parameters.AddWithValue("@role", int.Parse(comboRole.SelectedItem.Value.ToString()));
+                    cmd.Parameters.AddWithValue("@staffId", int.Parse(comboStaff.SelectedItem.Value));
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    literalActionSuccess.Text = "Role assignment successful. Page will refresh in 3 seconds.";
+                    Response.AddHeader("REFRESH", "3;");
+                }
+            }
+        }
+    }
+}
